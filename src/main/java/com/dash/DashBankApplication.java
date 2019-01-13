e65a7569a008195245dced74e4e84060b9929fc4
Arzab Jain <arzabjain@gmail.com>
2019-01-13 19:08:12 +0530

    initial commit

---
+++ src/main/java/com/dash/DashBankApplication.java
@@ -0,0 +1,14 @@
+package com.dash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashBankApplication.class, args);
+	}
+
+}
+
